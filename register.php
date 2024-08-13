fd59155a2a775d15d1f7c807d3115f0c99c39539
MEYYARASU A <projectbuilders.eng@gmail.com>
2024-08-13 21:30:34 +0530

    gi

--- register.php
+++ register.php
@@ -1,14 +1,50 @@
-<?php
-require 'config.php';
+<?php 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+include 'connect.php';
 
-    $stmt = $pdo->prepare('INSERT INTO users (username, email, password) VALUES (?, ?, ?)');
-    $stmt->execute([$username, $email, $password]);
+if(isset($_POST['signUp'])){
+    $firstName=$_POST['fName'];
+    $lastName=$_POST['lName'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $password=md5($password);
 
-    header('Location: login.html');
+     $checkEmail="SELECT * From users where email='$email'";
+     $result=$conn->query($checkEmail);
+     if($result->num_rows>0){
+        echo "Email Address Already Exists !";
+     }
+     else{
+        $insertQuery="INSERT INTO users(firstName,lastName,email,password)
+                       VALUES ('$firstName','$lastName','$email','$password')";
+            if($conn->query($insertQuery)==TRUE){
+                header("location: index.php");
+            }
+            else{
+                echo "Error:".$conn->error;
+            }
+     }
+   
+
+}
+
+if(isset($_POST['signIn'])){
+   $email=$_POST['email'];
+   $password=$_POST['password'];
+   $password=md5($password) ;
+   
+   $sql="SELECT * FROM users WHERE email='$email' and password='$password'";
+   $result=$conn->query($sql);
+   if($result->num_rows>0){
+    session_start();
+    $row=$result->fetch_assoc();
+    $_SESSION['email']=$row['email'];
+    header("Location: homepage.php");
+    exit();
+   }
+   else{
+    echo "Not Found, Incorrect Email or Password";
+   }
+
 }
 ?>
